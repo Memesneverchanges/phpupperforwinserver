2e5ea9e97dafb5c363ad2433b6c5dd8179bbaacf
Memesneverchanges <92294751+Memesneverchanges@users.noreply.github.com>
2022-02-09 19:24:49 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+try
+{
+    //new Thread(() =>
+    //{
+    //    while (true)
+    //    {
+    //        Console.WriteLine("open notepad");
+    //        Console.WriteLine(Directory.GetCurrentDirectory());
+    //        Process.Start("notepad.exe").WaitForExit();
+    //    }
+
+    //}).Start();
+
+    new Thread(() =>
+    {
+        //while (true)
+        //{ }
+        Console.WriteLine(@"cd " + Directory.GetCurrentDirectory() + " & start.bat");
+        Process myProcess = new Process();
+        myProcess.StartInfo.FileName = "cmd.exe";
+        myProcess.StartInfo.Arguments = @"cd " + Directory.GetCurrentDirectory() + "& start.bat";
+        myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+        myProcess.StartInfo.CreateNoWindow = true;
+        myProcess.Start();
+
+
+    }).Start();
+
+
+}
+catch (Exception ex)
+{
+    System.Console.WriteLine(ex.ToString());
+}
